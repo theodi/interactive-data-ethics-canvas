0998c308ce26d712ed8bd233ce572db87909ccea
Giles Dring <giles@dringtech.com>
2021-02-08 11:04:45 +0000

    feature: add editor blob

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,11 +1,13 @@
 <script>
   import Blob from './Blob.svelte';
   import { canvasState } from '../store';
+  import { fade } from 'svelte/transition'
   const cols = 5;
   const rows = 3;
   const areaConfig = { size: 200, overlap: 0.05 };
   const margin = areaConfig.size * areaConfig.overlap * 5;
   const height = rows * areaConfig.size;
+  $: focusBlob = $canvasState.findIndex((x) => x.focussed === true);
 </script>
 
 <svg viewBox="-{ margin } -{ margin } { cols * areaConfig.size + 2 * margin} { height + 2 * margin }">
@@ -25,13 +27,38 @@
       canvasHeight={ height }
     />
   {/each}
+
+  {#if focusBlob > -1}
+  <g class="editor">
+    <rect in:fade class="blank" x={-margin} y={-margin} width={ cols * areaConfig.size + 2 * margin} height={ height + 2 * margin } />
+    <Blob 
+      { ...$canvasState[focusBlob] }
+      { ...areaConfig }
+      canvasHeight={ height }
+    />
+    <g class="button" transform="translate({ cols * areaConfig.size - 50 } { 0 })" on:click={ () => $canvasState[focusBlob].focussed = false } >
+      <rect x=0 y=0 width=50 height=50 />
+      <line x1=10 y1=10 x2=40 y2=40 />
+      <line x1=10 y1=40 x2=40 y2=10 />
+    </g>
+  </g>
+  {/if}
+  
 </svg>
 
 <style>
   svg {
     background-color: #eee;
   }
+  .blank {
+    fill: white;
+  }
   .cloud {
+    fill: white;
+  }
+  .button {
+    stroke: black;
+    stroke-width: 5;
     fill: white;
   }
 </style>
