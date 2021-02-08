7fde6381fe2b296404b6be9efe344197389651f9
Giles Dring <giles@dringtech.com>
2021-02-08 22:34:39 +0000

    feature: set uuid on canvas store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { writable } from 'svelte/store';
 import { baseLayout } from './templates/canvas-base';
+import { v4 as uuid } from 'uuid';
 
 const canvas = () => {
   const { subscribe, set, update } = writable({ blobs: baseLayout });
+  const setId = () =>
+    update((s) => {
+      s.uuid = uuid();
+      return s;
+    });
+
+  setId();
 
   return {
     subscribe,
