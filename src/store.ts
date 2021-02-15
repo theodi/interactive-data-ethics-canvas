7c0e202a10bcac94e5b9b20f6b9f17ce6d0ab2a8
Giles Dring <giles@dringtech.com>
2021-02-15 10:15:32 +0000

    Rough out methods for the store interactions

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,12 @@ import { writable } from 'svelte/store';
 import { baseLayout } from './templates/canvas-base';
 import { v4 as uuid } from 'uuid';
 import type { Canvas } from './types/canvas';
+import type { Blob } from './types/blob';
+
+interface CanvasDefinition {
+  uuid?: string;
+  blobs: Blob[];
+}
 
 const canvas = () => {
   const { subscribe, set, update } = writable({ blobs: baseLayout });
@@ -13,10 +19,25 @@ const canvas = () => {
 
   setId();
 
+  const loadCanvas = ({ blobs, uuid }: CanvasDefinition) => {
+    // if uuid defined set id to that
+    // set the blobs to the passed in blobs
+    // use the set function to completely override state
+  }
+
+  function serialiseCanvas(): CanvasDefinition {
+    // construct a serialised only including state that we want to save.
+    // e.g. each blob should not include hovered, focused, etc;
+    // state is available as `this`
+    return;
+  }
+
   return {
     subscribe,
     set,
     update,
+    loadCanvas,
+    serialise: serialiseCanvas,
   };
 };
 
