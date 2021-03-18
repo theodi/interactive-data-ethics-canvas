ec64e7cf30e224eea534702f56186a887b078369
danbillingsley <danielbillingsley85@gmail.com>
2021-03-18 14:38:32 +0000

    Added upload canvas functionality

--- src/components/Upload.svelte
+++ src/components/Upload.svelte
@@ -1,3 +1,31 @@
+<script>
+  let fileJSON, fileinput;
+  import { canvasState, canvasReviver } from '../store';
+
+	const onFileSelected =(e)=>{
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = e => {
+      fileJSON = JSON.parse(e.target.result, canvasReviver);
+      canvasState.loadCanvas(fileJSON);
+    };
+  }
+</script>
+
 <div>
-  <p>Upload</p>
+  <h2>Upload canvas</h2>
+  <input type="file" accept=".json" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
 </div>
+
+<style>
+  h2 {
+    font-weight: bold;
+    margin: 1em 0.3em 0.5em 0.3em;
+  }
+
+  input {
+    margin: 0 0.3em 2em 0.3em;
+  }
+
+</style>
