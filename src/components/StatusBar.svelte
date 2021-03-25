ffca709a898be417ab305194bbc4c8ff0f6a09f4
Giles Dring <giles@dringtech.com>
2021-03-25 18:19:51 +0000

    Fix styling on status bar

--- src/components/StatusBar.svelte
+++ src/components/StatusBar.svelte
@@ -1,71 +1,58 @@
-<script>
+<script lang='typescript'>
   import { lastUpdate } from '../store';
   import { canvasState } from '../store';
   import { getLocalization } from '../i18n';
   import { Status } from '../types';
   const { t } = getLocalization();
-
-  let blobStatuses;
-  $: {
-    blobStatuses = $canvasState.blobs.map(blob => blob.status);
-  }
+  const src = 'images/CC-BY-SA_icon_white.png';
 
   function getOverallCanvasStatus(statuses) {
     for (const status in Status) {
       if (statuses.indexOf(status) > -1) {
-        return $t('status:' + status);
+        return status;
       }
     }
   }
 
-  let src = 'images/CC-BY-SA_icon_white.png';
-
+  let blobStatus: string;
+  $: {
+    const blobStatuses = $canvasState.blobs.map(blob => blob.status);
+    blobStatus = getOverallCanvasStatus(blobStatuses);
+  }
 </script>
 
 <ul>
-<li><span class="left">Last saved: { $lastUpdate.toLocaleString() }<br>Status: { getOverallCanvasStatus(blobStatuses) }</span><span class="right"><span class="about">Data Ethics Canvas by <a href="https://theodi.org">The Open Data Institute</a> is licensed under a <br><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a></span><img src={src} alt="cc"/></span></li>
-</ul> 
+  <li class="grow">
+    Last saved: { $lastUpdate.toLocaleString() }<br>Status: { $t( 'status:' + blobStatus ) }
+  </li>
+  <li class="license">
+    <span class="about">Data Ethics Canvas by <a href="https://theodi.org">The Open Data Institute</a> is licensed under a <br>
+    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>
+    </span>
+    <img {src} alt='cc-by-sa license' />
+  </li>
+</ul>  
+
 <style>
   ul {
     display: flex;
     width: 100%;
-    padding: 1em;
+    padding: 0.5em 1em;
     color: var(--pale-grey);
     background: var(--dark-blue);
+    line-height: 1.2;
   }
-
-  li {
-    width: 100%;
+  .grow {
+    flex-grow: 1;
   }
-
-  .left {
-    float: left;
+  .license {
+    display: flex;
   }
-
-  .right {
-    float: right;
-  }
-
-  .right a {
+  a {
     color: inherit;
   }
-
-  .right .about {
-    width: 400px;
-    display: inline-block;
-    vertical-align: top;
-    padding-top: 3px;
-  }
-
-  .right img {
+  img {
     height: 32px;
-    display: inline-block;
-  }
-
-  @media screen and (max-width: 800px) {
-    .right {
-      margin-top: 1em;
-      float: left;
-    }
+    padding-left: 1em;
   }
 </style>
