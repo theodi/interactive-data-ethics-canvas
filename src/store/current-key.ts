452bd65b71a17983c67ac787b2e78807c80609c6
Giles Dring <giles@dringtech.com>
2021-04-01 21:51:49 +0000

    Fix implementation of current key storage

--- src/store/current-key.ts
+++ src/store/current-key.ts
@@ -4,3 +4,5 @@ import { v4 as uuid } from 'uuid';
 export const CURRENT_KEY = 'canvas_current_key';
 
 export const currentKey = writable(localStorage.getItem(CURRENT_KEY) || uuid());
+
+currentKey.subscribe(k => localStorage.setItem(CURRENT_KEY, k));
